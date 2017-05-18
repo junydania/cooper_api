3791faafcce67ea3a07c358755a8d6bde024ff41
junydania <o.dania@aol.com>
2017-05-18 12:40:16 +0200

    removed files not required from the api folder

--- src/Person.js
+++
@@ -1,8 +0,0 @@
-function Person(attr) {
-  this.gender = attr.gender;
-  this.age = attr.age;
-}
-
-Person.prototype.assessCooper = function (distance) {
-  this.cooperMessage = cooperAssessmentOf(this, distance);
-};
